73bbd2894013baada408da46226da0504c481672
seancorcoran21 <x00189029@mytudublin.ie>
2025-07-26 02:33:08 +0100

    sale, templates, about page

--- store/views.py
+++ store/views.py
@@ -4,3 +4,7 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
+
+
+def about(request):
+    return render(request, 'about.html', {})
